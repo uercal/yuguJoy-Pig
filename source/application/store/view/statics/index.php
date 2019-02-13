7b7db7fe6496a7dcee6f39ae835c46ce926d8b0d
uercal <317215138@qq.com>
2019-02-13 10:03:04 +0800

    Signed-off-by: uercal <317215138@qq.com>

--- source/application/store/view/statics/index.php
+++ source/application/store/view/statics/index.php
@@ -49,7 +49,7 @@
         legend: {
             orient: 'vertical',
             left: 'left',
-            data: JSON.parse('<?= json_encode($pies['legend_data']) ?>')
+            data: JSON.parse("<?= isset($pies['legend_data'])?json_encode($pies['legend_data']):json_encode([]) ?>")
         },
         series : [
             {
